9ec522cf893da393bafd67613dcb8d97b8c50a42
Hjordten <andr314e@stud.kea.dk>
2022-09-09 10:57:30 +0200

    added askAboutAge

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class SuperGreeter {
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
         sg.greet();
+        sg.askAboutAge();
     }
 
     public void start() {
@@ -21,6 +23,15 @@ public class SuperGreeter {
 
     }
 
+    public void askAboutAge() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indtast din alder");
 
+        int age = sc.nextInt();
+
+        System.out.println(age + " Hold kæft du er gammel");
+
+
+    }
 
 }
